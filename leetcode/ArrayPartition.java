df6642dcb920b6528689a97a046b801ca1a80ce7
dpyorang <dpyorang@gmail.com>
2025-06-06 23:55:43 +0900

    Algorithm

---
+++ leetcode/ArrayPartition.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArrayPartition {
+    public static void main(String[] args) {
+
+    }
+
+    public int arrayPairSum(int[] nums) {
+        int result = 0;
+
+        Arrays.sort(nums);
+
+
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            result += Math.min(nums[i], nums[i + 1]);
+        }
+
+        return result;
+    }
+}
